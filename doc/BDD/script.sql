cf05b13a1ed6071e99daf8ccc81efb97d324c0f5
Srekaens <justindidelot8@outlook.fr>
2024-11-21 11:17:26 +0100

    chore(script.sql): add new command in script.sql
    add command for create database

---
+++ doc/BDD/script.sql
@@ -0,0 +1,13 @@
+-- Script SQL pour la création de la base de données PostgreSQL
+-- Auteur : Justin DIDELOT
+-- Date : 21/11/2024
+
+-- ========================================
+-- 1. Création de la base de données
+-- ========================================
+
+CREATE DATABASE simpluedo;
+
+
+
+
